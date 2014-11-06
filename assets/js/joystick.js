68a06b3adb7c67e37c84e21acf77feedf7648472
Ross Lehr <itsme@rosslehr.com>
2014-11-06 15:22:07 -0500

    Update joystick.js
    Add comments

--- assets/js/joystick.js
+++ assets/js/joystick.js
@@ -1,13 +1,9 @@
-/**
-* Holds all of the code for the game states play
-*
-* @class Game.States.Play
-*/
+
 
 var game = new Phaser.Game(1024, 672, Phaser.CANVAS, 'phaser-example',{ preload: preload, create: create, update: update, render: render });
 
 	function preload() 
-    {
+    	{
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		
 		this.scale.minWidth = 480;                                                                                                                      
@@ -25,57 +21,67 @@ var game = new Phaser.Game(1024, 672, Phaser.CANVAS, 'phaser-example',{ preload:
 		
 		this.input.addPointer();
 		
-     	game.load.image('grid', 'assets/images/grid.jpg');
+		//Load the images
+     		game.load.image('grid', 'assets/images/grid.jpg');
 		game.load.image('rocket', 'assets/images/rocketShip.png');
 		game.load.image('base', 'assets/images/gamePad/joystick-base.png');
 		game.load.image('stick', 'assets/images/gamePad/joystick-stick.png');
 		
-    }
+    	}
 	
 	function create() 
-    {		
-		
-        this.game.world.setBounds(0, 0, 1200, 1200);
-        
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-        
-        this.game.add.sprite(0,0,'grid');
-        
-        this.rocket = this.game.add.sprite((this.game.camera.width/2),(this.game.world.height/2)-50,'rocket');
-        this.rocket.anchor.setTo(0.5,0.5);
-        
-        this.game.physics.p2.enable(this.rocket);
-        this.game.camera.follow(this.rocket);
-                        
-        //Add the game interface
-        this.gameInterface = new GameInterface(this.game,2,true);
-        this.gameInterface.fixedToCamera();
+    	{		
+		//Set the world bounds
+	        this.game.world.setBounds(0, 0, 1200, 1200);
+	        
+	        //Add physics
+	        this.game.physics.startSystem(Phaser.Physics.P2JS);
+	        
+	        //Add the background grid
+	        this.game.add.sprite(0,0,'grid');
+	        
+	        //Add the rocket
+	        this.rocket = this.game.add.sprite((this.game.camera.width/2),(this.game.world.height/2)-50,'rocket');
+	        this.rocket.anchor.setTo(0.5,0.5);
+	        
+	        //add the rocket to the physics
+	        this.game.physics.p2.enable(this.rocket);
+	        this.game.camera.follow(this.rocket);
+	                        
+	        //Add the game interface
+	        this.gameInterface = new GameInterface(this.game,2,true);
+	        this.gameInterface.fixedToCamera();
 		
 	}
 
 	function update() 
-    {
-		console.log("update");
-        this.gameInterface.update();
-        
-        this.rocket.body.setZeroVelocity();
-        
-        this.baseSpeed = 1500;
-        
-        var horzVal = this.gameInterface.getHorzVal();
-        
-        var vertVal = this.gameInterface.getVertVal();
-        
-        this.rocket.body.moveRight(this.baseSpeed * horzVal);
-        
-        this.rocket.body.moveDown(this.baseSpeed * vertVal);
+    	{
+		
+	        this.gameInterface.update();
+	        
+	        this.rocket.body.setZeroVelocity();
+	        
+	        //Set the base speed of the rocket
+	        this.baseSpeed = 1500;
+	        
+	        //Get the joysticks hoizontal value (-1 to 1)
+	        var horzVal = this.gameInterface.getHorzVal();
+	        
+	        //Get the joysticks verticle value (-1 to 1)
+	        var vertVal = this.gameInterface.getVertVal();
+	        
+	        //move the rocket horizontally
+	        this.rocket.body.moveRight(this.baseSpeed * horzVal);
+	        
+	        //move the rocket vertically
+	        this.rocket.body.moveDown(this.baseSpeed * vertVal);
     
 				
 	}
 	
-    function render()
-    {
-        //this.game.debug.cameraInfo(this.game.camera, 32, 32);
-        //this.game.debug.spriteCoords(this.rocket, 32, 200);
-    }
+	    function render()
+	    {
+	        //this.game.debug.cameraInfo(this.game.camera, 32, 32);
+	        //this.game.debug.spriteCoords(this.rocket, 32, 200);
+	    }
 
